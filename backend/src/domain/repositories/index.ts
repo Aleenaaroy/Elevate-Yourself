a2ff115f84e70139c4dcc08f669b9e5bf9e669a8
Aleenaaroy <aleenaaroy@gmail.com>
2024-10-15 12:21:30 +0530

    optimizing admincontroller

---
+++ backend/src/domain/repositories/index.ts
@@ -0,0 +1,7 @@
+export { AdminRepository } from './AdminRepository';
+export { CategoryRepository } from './CategoryRepository';
+export { CompanyRepository } from './CompanyRepository';
+export { JobRepository } from './JobRepository';
+export { NotificationRepository } from './NotificationRepository';
+export { PostRepository } from './PostRepository';
+export { UserRepository } from './UserRepository';
---
+++ backend/src/domain/repositories/interfaces/index.ts
@@ -0,0 +1,7 @@
+export { IAdminRepository } from './IAdminRepository';
+export { ICategoryRepository } from './ICategoryRepository';
+export { ICompanyRepository } from './ICompanyRepository';
+export { IJobRepository } from './IJobRepository';
+export { INotificationRepository } from './INotificationRepository';
+export { IPostRepository } from './IPostRepository';
+export { IUserRepository } from './IUserRepository';
